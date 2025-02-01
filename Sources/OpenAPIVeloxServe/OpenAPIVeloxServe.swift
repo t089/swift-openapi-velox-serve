0b2ca89b6571dfc07526608981686d1a8e577459
Tobias Haeberle <tobias.haeberle@me.com>
2025-02-01 11:57:47 +0100

    provide the request logger in a task local to the handler

--- Sources/OpenAPIVeloxServe/OpenAPIVeloxServe.swift
+++ Sources/OpenAPIVeloxServe/OpenAPIVeloxServe.swift
@@ -2,6 +2,13 @@ import OpenAPIRuntime
 import VeloxServe
 import HTTPTypes
 import AsyncAlgorithms  
+import Logging
+
+public struct RequestContext: Sendable {
+    @TaskLocal public static var current: RequestContext?
+    
+    public let logger: Logger
+}
 
 public final class RouterTransport :  ServerTransport {
     private var router: Router
@@ -29,10 +36,13 @@ public final class RouterTransport :  ServerTransport {
             let length: HTTPBody.Length = req.headers[.contentLength].flatMap(Int64.init).map { HTTPBody.Length.known($0) } ?? .unknown
             let body = HTTPBody(reqBodyChannel, length: length, iterationBehavior: .single)
             let metadata = ServerRequestMetadata(pathParameters: req.pathParameters.params)
+            let logger = req.logger
             
             try await withThrowingTaskGroup(of: (HTTPResponse, HTTPBody?).self) { group in
                 group.addTask  { 
-                    try await handler(request, body, metadata)
+                    try await RequestContext.$current.withValue(RequestContext(logger: logger)) {
+                        try await handler(request, body, metadata)
+                    }
                 }
 
                 do {
